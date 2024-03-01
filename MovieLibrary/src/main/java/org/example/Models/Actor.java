9af0c179f8ae9f82c5d1946ff8fa2aaed1446bef
beata.dobko <beata.dobko@gmail.com>
2024-03-01 14:41:54 +0100

    add Actor class

---
+++ MovieLibrary/src/main/java/org/example/Models/Actor.java
@@ -0,0 +1,13 @@
+package org.example.Models;
+
+public class Actor {
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
